73b68e1a723aef7e278411da15dd941d99573b99
Nefedov Ivan <atx-exe@mail.ru>
2022-12-25 18:22:17 +0300

    Add movements

--- tetramino.py
+++ tetramino.py
@@ -1,4 +1,5 @@
 from setting import *
+import random
 
 
 class Block(pg.sprite.Sprite):
@@ -9,15 +10,25 @@ class Block(pg.sprite.Sprite):
         super().__init__(tetramino.tetris.sprite_group)
         self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
         self.image.fill('orange')
-
         self.rect = self.image.get_rect()
+
+    def set_rect_pos(self):
         self.rect.topleft = self.pos * TILE_SIZE
+
+    def update(self):
+        self.set_rect_pos()
 
 class Tetromino:
     def __init__(self, tetris):
         self.tetris = tetris
-        self.shape = 'T'
+        self.shape = random.choice(list(TETROMINOES.keys()))
         self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
 
+    def move(self, direction):
+        move_direction = MOVE_DIRECTIONS[direction]
+        for block in self.blocks:
+            block.pos += move_direction
+
     def update(self):
-        pass
+        self.move(direction='down')
+        pg.time.wait(200)
